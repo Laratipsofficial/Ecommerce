a07d8a568621beaeaeb8825c3fe43d2c259d0932
Laratips <aashish201030@gmail.com>
2022-07-14 15:06:59 +0100

    split vendor and main app chunks

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
@@ -16,6 +16,7 @@ export default defineConfig({
         },
       },
     }),
+    splitVendorChunkPlugin(),
   ],
   resolve: {
     alias: {
